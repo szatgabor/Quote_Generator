afdeb423ac5c2cad4fae16dd8d5ef9d5b6fb6c1d
szatgabor <gabor.samers@gmail.com>
2021-04-06 11:32:57 +0200

    loader added

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //show New Quote
 function newQuote(){
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Chekc if author field is blank and replace it with "unknown"
@@ -22,11 +36,14 @@ function newQuote(){
     } else{
         quoteText.classList.remove('long-quote');
     }
+    //Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 //Get quotes from API
 async function getQuotes(){
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
