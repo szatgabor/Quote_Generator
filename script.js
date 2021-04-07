480ea7115e05eaf6a54f577d96d7de2043a84886
szatgabor <gabor.samers@gmail.com>
2021-04-07 12:34:57 +0200

    external API implemented, cleaning code, finishing the app

--- script.js
+++ script.js
@@ -5,53 +5,64 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-let apiQuotes = [];
-
-//Show Loading
-function loading(){
+function showLoadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-//Hide loading
-function complete(){
+//Loading spinner OFF
+function removeLoadingSpinner(){
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
-//show New Quote
-function newQuote(){
-    loading();
-    //Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    //Chekc if author field is blank and replace it with "unknown"
-    if (!quote.author){
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    //Check the Quote length to determine styling
-    if (quote.text.length > 120){
-        quoteText.classList.add('long-quote');
-    } else{
-        quoteText.classList.remove('long-quote');
-    }
-    //Set Quote, Hide Loader
-    quoteText.textContent = quote.text;
-    complete();
-}
+/*show New Quote Function for local APIs 
+
+ function newQuote(){
+     loading();
+     //Pick a random quote from apiQuotes array
+     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+     //Check if author field is blank and replace it with "unknown"
+     if (!quote.author){
+         authorText.textContent = 'Unknown';
+     } else {
+         authorText.textContent = quote.author;
+     }
+     //Check the Quote length to determine styling
+     if (quote.text.length > 120){
+         quoteText.classList.add('long-quote');
+     } else{
+         quoteText.classList.remove('long-quote');
+     }
+     //Set Quote, Hide Loader
+     quoteText.textContent = quote.text;
+     complete();
+ }*/
 
 //Get quotes from API
 async function getQuotes(){
-    loading();
-    const apiUrl = "https://type.fit/api/quotes";
+    showLoadingSpinner();
+    const proxyUrl = 'https://polar-everglades-12070.herokuapp.com/';
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try{
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote()
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If author is blank, add 'Unkown'
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        if (data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        removeLoadingSpinner();
     } catch(error){
-        alert(error);
-        //Catch Error Here
+        getQuotes()
+
     }
 }
 
@@ -63,6 +74,6 @@ function tweetQuote(){
 
 // Event listeners
 twitterBtn.addEventListener('click', tweetQuote);
-newQuoteBtn.addEventListener('click', newQuote);
+newQuoteBtn.addEventListener('click', getQuotes);
 //On Load
 getQuotes();
